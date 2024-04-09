f92c7cc6b1ee11395df36b56130b074f6b34b909
Jan Strahlmann <coolflyingtukan@gmail.com>
2024-04-09 20:20:59 +0200

    Join Animation

--- js/logIn.js
+++ js/logIn.js
@@ -3,3 +3,20 @@ function toggleShowPassword() {
     const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
     passwordInput.setAttribute('type', type);
 }
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    setTimeout(() => {
+      const bild = document.getElementById('meinBild');
+      const ziel = document.getElementById('zielbereich');
+      const inhalt = document.getElementById('inhalt');
+      const zielRect = ziel.getBoundingClientRect();
+      bild.style.width = '100px'; 
+      bild.style.top = zielRect.top + 'px';
+      bild.style.left = zielRect.left + 'px';
+  
+      setTimeout(() => {
+        inhalt.style.opacity = 1; 
+        inhalt.style.filter = 'blur(0px)'; 
+      }, 1000); 
+    }, 1000); 
+  });
