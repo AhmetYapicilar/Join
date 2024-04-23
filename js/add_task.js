6b1e2623be4fc5e1d5a19fd551cebadd30e2e052
BastianThoma <ossi_1993@hotmail.de>
2024-04-23 20:10:49 +0200

    added PrioPicker Functions, some CSS Changes

--- js/add_task.js
+++ js/add_task.js
@@ -11,18 +11,33 @@ function createTask() {
         priority = getPriority(),
         category = document.querySelector(".categoryPicker").value,
         subtasks = getSubtasks(),
-        task = {title, description, assignedTo, dueDate, priority, category, subtasks}; 
-    if (title.trim() === "" || dueDate.trim() === "" || category.trim() === "") {
-        return alert("Please fill in all required fields.");
-    }
+        task = { title, description, assignedTo, dueDate, priority, category, subtasks };
+
     setItem(task, title);
 }
 
-function selectPriority(priority) {
+function resetButtons() {
     document.querySelectorAll('.urgentButton, .mediumButton, .lowButton').forEach(button => {
         button.classList.remove('urgentButtonSelected', 'mediumButtonSelected', 'lowButtonSelected');
+        const img = button.querySelector('img');
+        switch (button.id) {
+            case 'low':
+                img.src = './assets/img/prioDown.png';
+                break;
+            case 'medium':
+                img.src = './assets/img/prioEven.png';
+                break;
+            case 'urgent':
+                img.src = './assets/img/prioUp.png';
+                break;
+            default:
+                break;
+        }
     });
+}
 
+function selectPriority(priority) {
+    resetButtons();
     const selectedButton = document.getElementById(priority);
     const img = selectedButton.querySelector('img');
     switch (priority) {
@@ -39,6 +54,35 @@ function selectPriority(priority) {
             break;
     }
     selectedButton.classList.add(`${priority}ButtonSelected`);
+}
+
+function getPriority() {
+    let priority;
+    if (document.getElementById('urgent').classList.contains('urgentButtonSelected')) {
+        priority = 'urgent';
+    } else if (document.getElementById('medium').classList.contains('mediumButtonSelected')) {
+        priority = 'medium';
+    } else if (document.getElementById('low').classList.contains('lowButtonSelected')) {
+        priority = 'low';
+    } else {
+        priority = 'medium';
+    }
+    return priority;
+}
+
+function selectMedium() {
+    resetButtons();
+    selectPriority('medium');
+}
+
+function onLoad() {
+    selectMedium();
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', onLoad);
+} else {
+    onLoad();
 }
 
 function addSubtask() {
